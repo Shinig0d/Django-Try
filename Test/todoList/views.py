7468d8190c37ab62c0ebb30d3fb6550c0f3f247e
Shinig0d <pranilshrestha2016@gmail.com>
2023-06-06 13:59:53 +0545

    todoList Complete

--- Test/todoList/views.py
+++ Test/todoList/views.py
@@ -1,15 +1,33 @@
 from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 tasks = ["Do Hw", "Dance"]
 
 # Create your views here.
-
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
 
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+
     return render(request, "todoList/index.html", {
-        "tasks":tasks
+        "tasks":request.session["tasks"]
     })
 
 def add(request):
-    return render(request, "todoList/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("todoList:index"))
+        else:
+            return render(request, "todoList/add.html", {
+                "form": form
+            })
+    return render(request, "todoList/add.html", {
+        "form":NewTaskForm()
+    })
